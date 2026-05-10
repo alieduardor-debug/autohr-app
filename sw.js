ef8fdc39252d70a78ebf79b85258470bf863d6d4
alieduardor-debug <alieduardor@gmail.com>
2026-05-10 14:03:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,36 @@
-var CACHE = 'autohr-v13';
+// Auto H&R Service Worker v14
+// Estrategia: network-first sin cache para HTML, siempre fresco
+var CACHE_NAME = 'autohr-v14';
 
 self.addEventListener('install', function(e){
-  self.skipWaiting();
+  // Borrar todo cache anterior e instalar inmediatamente
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.map(function(k){ return caches.delete(k); }));
+    }).then(function(){ return self.skipWaiting(); })
+  );
 });
 
 self.addEventListener('activate', function(e){
   e.waitUntil(
     caches.keys().then(function(keys){
       return Promise.all(keys.map(function(k){ return caches.delete(k); }));
-    })
+    }).then(function(){ return self.clients.claim(); })
   );
-  self.clients.claim();
 });
 
-// Network-first: siempre red, sin cache de HTML
-// Asi el iPhone siempre ve la version mas reciente
 self.addEventListener('fetch', function(e){
+  // JSONBin: siempre red, nunca cache
   if(e.request.url.includes('jsonbin.io')){
-    e.respondWith(fetch(e.request).catch(function(){ return new Response('{}'); }));
+    e.respondWith(
+      fetch(e.request, {credentials:'omit'})
+        .catch(function(){ return new Response('{}',{headers:{'Content-Type':'application/json'}}); })
+    );
     return;
   }
-  // Para el HTML: siempre red, sin cache
-  e.respondWith(fetch(e.request).catch(function(){ return new Response('App sin conexion. Conectate a internet.'); }));
+  // Todo lo demas: siempre red (no cache), para garantizar version mas reciente
+  e.respondWith(
+    fetch(e.request)
+      .catch(function(){ return new Response('Sin conexion. Recarga cuando tengas internet.'); })
+  );
 });
